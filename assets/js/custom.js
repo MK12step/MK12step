0db54dc0de3a533701188ca783ee303dd49044ee
Nick.Anon.UK <nick.anon.29@gmail.com>
2021-10-16 15:33:33 +0100

    fix: SASS/CSS style fixes - letter-spacing, accessibility (#1)
    * Plus, accessibility fix via Javascript

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,6 @@
 
+/* 7th tradition "Donate" button.
+*/
 const CFG = document.querySelector('[data-paypal-pool-url]').dataset;
 const NAV = document.querySelector('.site-nav .trigger');
 
@@ -7,6 +9,22 @@ NAV.innerHTML += `<a
   target="_blank"
   title="7th tradition contributions (Opens in a new window)"
   >Donate</a>
-`;
+`; // ☞;
+
+/* Accessibility fix!
+*/
+const TOGGLE = document.querySelector('.site-nav [ for = nav-trigger ]');
+const CHECK  = document.querySelector('.site-nav .nav-trigger');
+
+TOGGLE.setAttribute('tabindex', 0);
+TOGGLE.role = 'button';
+TOGGLE.title = 'Toggle navigation';
+TOGGLE.onkeypress = ev => {
+  ev.preventDefault();
+
+  console.debug('Toggle:', CHECK, CHECK.checked, ev);
+
+  CHECK.checked = CHECK.checked ? '' : 'checked';
+};
 
 console.debug('custom.js:', CFG);
